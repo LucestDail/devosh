3da1092c77ac7f79b6b74d8298d720fca6515280
hyun <dhtmdgussksm@naver.com>
2021-02-15 20:56:15 +0900

    update file
    change some html, css templates,
    change sqlite columns

--- devosh/portfolio/models.py
+++ devosh/portfolio/models.py
@@ -4,21 +4,10 @@ from django.db import models
 
 
 class Project(models.Model):
-    title = models.CharField(max_length=50)
-    titleimg = models.ImageField(blank=True, null=True)
+    title = models.CharField(max_length=100)
+    git = models.CharField(max_length=100)
+    demo = models.CharField(max_length=100)
+    img = models.ImageField(blank=True, null=True)
     introduction = models.TextField()
-    subtitle1 = models.CharField(max_length=50)
-    subimg1 = models.ImageField(blank=True, null=True)
-    subcontents1 = models.TextField()
-    subtitle2 = models.CharField(max_length=50)
-    subimg2 = models.ImageField(blank=True, null=True)
-    subcontents2 = models.TextField()
-    subtitle3 = models.CharField(max_length=50)
-    subimg3 = models.ImageField(blank=True, null=True)
-    subcontents3 = models.TextField()
-    conclusiontitle = models.CharField(max_length=50)
-    conclusionimg = models.ImageField(blank=True, null=True)
-    conclusioncontents = models.TextField()
-
     def __str__(self):
         return self.title
