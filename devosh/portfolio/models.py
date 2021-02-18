e55ba1e34a2f691318db723690af9511608fb62d
hyun <dhtmdgussksm@naver.com>
2021-02-18 10:37:45 +0900

    img src type change

--- devosh/portfolio/models.py
+++ devosh/portfolio/models.py
@@ -7,7 +7,7 @@ class Project(models.Model):
     title = models.CharField(max_length=100)
     git = models.CharField(max_length=100)
     demo = models.CharField(max_length=100)
-    img = models.ImageField(blank=True, null=True)
+    img = models.CharField(max_length=100)
     introduction = models.TextField()
     def __str__(self):
         return self.title
